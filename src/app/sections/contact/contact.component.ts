7ed8bd01abca0c52a7c05c688138f0b77b9b0cea
ManzanoYG <manzanoygonzalezmathias.pro@gmail.com>
2025-07-02 15:06:42 +0200

    Create a basic contact form

---
+++ src/app/sections/contact/contact.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.css'
+})
+export class ContactComponent {
+  isSubmitting = false;
+  successMessage = '';
+
+  onSubmit() {
+    if (this.isSubmitting) return;
+
+    this.isSubmitting = true;
+    this.successMessage = '';
+
+    // Simuler envoi (ex: API)
+    setTimeout(() => {
+      this.isSubmitting = false;
+      this.successMessage = "Merci pour votre message, nous vous répondrons bientôt !";
+    }, 2000);
+  }
+}
