09c126893ec743a194d9fafe3ee99ca8a75604b1
ManzanoYG <manzanoygonzalezmathias.pro@gmail.com>
2025-07-08 17:16:01 +0200

    display footer inside main content and design it

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  currentYear: number = new Date().getFullYear();
+  
+  constructor(private translate: TranslateService) {}
 
+  ngOnInit(): void {
+    // Initialisation, si nécessaire
+  }
 }
