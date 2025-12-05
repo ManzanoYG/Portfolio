d1addb7cec197edc1f72378cb0211b032218edc5
ManzanoYG <manzanoygonzalezmathias.pro@gmail.com>
2025-12-05 15:42:31 +0100

    Add basic information and social media

--- src/app/sections/profile/profile.component.ts
+++ src/app/sections/profile/profile.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
+  birthDate: Date = new Date(2000, 3, 21);
+  showLinkedInText = false;
+  showGitHubText = false;
+  showDownloadText = false;
 
+  public getAge(): any {
+    const today = new Date();
+    let age = today.getFullYear() - this.birthDate.getFullYear();
+    const monthDifference = today.getMonth() - this.birthDate.getMonth();
+    if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < this.birthDate.getDate())) {
+      age--;
+    }
+    return age;
+  }
 }
