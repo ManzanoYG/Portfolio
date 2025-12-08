f0d087605fe3ac46b7fdbe60b6a3233f6f3d5709
ManzanoYG <manzanoygonzalezmathias.pro@gmail.com>
2025-12-08 16:16:34 +0100

    Design contact section and add translation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -59,6 +60,8 @@ export function HttpLoaderFactory(http: HttpClient) {
     MatSidenavModule,
     MatIconModule,
     MatListModule,
+    FormsModule,
+    ReactiveFormsModule,
     StoreModule.forRoot({}, {})
   ],
   providers: [
