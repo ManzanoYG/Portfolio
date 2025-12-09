abefe1acfb5c40594382ce9e504f2404d1495ad4
ManzanoYG <manzanoygonzalezmathias.pro@gmail.com>
2025-12-09 20:57:51 +0100

    Apply dark theme to the navbar main content and profile section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,8 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class AppComponent implements OnInit {
   title = 'Portfolio';
+  public lightTheme: boolean = false;
+
   constructor(private translate: TranslateService) {
   }
 
@@ -21,5 +23,33 @@ export class AppComponent implements OnInit {
       this.translate.use(defaultLang);
       localStorage.setItem('language', defaultLang);
     }
+  }
+
+
+  clickMenu(){
+    this.lightTheme = !this.lightTheme;
+
+
+    Array.from(document.getElementsByTagName('p')).forEach(element => {
+        element.classList.toggle('dark-theme');
+      });
+    Array.from(document.getElementsByTagName('h3')).forEach(element => {
+      element.classList.toggle('dark-theme');
+    });
+    Array.from(document.getElementsByTagName('a')).forEach(element => {
+      element.classList.toggle('dark-theme');
+    });
+    Array.from(document.getElementsByTagName('span')).forEach(element => {
+      element.classList.toggle('dark-theme');
+    });
+    Array.from(document.getElementsByTagName('i')).forEach(element => {
+      element.classList.toggle('dark-theme');
+    });
+    Array.from(document.getElementsByTagName('li')).forEach(element => {
+      element.classList.toggle('dark-theme');
+    });
+    Array.from(document.getElementsByTagName('div')).forEach(element => {
+      element.classList.toggle('dark-theme');
+    });
   }
 }
