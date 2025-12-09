306ac7b31f06ea10b81031ece9d5b96cd4ab2105
ManzanoYG <manzanoygonzalezmathias.pro@gmail.com>
2025-12-09 23:55:52 +0100

    Theme service to send actual theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { ThemeService } from './services/theme.service';
 
 @Component({
   selector: 'app-root',
@@ -10,8 +11,10 @@ export class AppComponent implements OnInit {
   title = 'Portfolio';
   public lightTheme: boolean = false;
 
-  constructor(private translate: TranslateService) {
-  }
+  constructor(
+    private translate: TranslateService,
+    private themeService: ThemeService
+  ) {}
 
   async ngOnInit() {
     const storedLang = localStorage.getItem('language');
@@ -28,6 +31,7 @@ export class AppComponent implements OnInit {
 
   clickMenu(){
     this.lightTheme = !this.lightTheme;
+    this.themeService.setTheme(this.lightTheme);
 
     Array.from(document.querySelectorAll('p, h1, h2, h3, h4, h5, h6, a, span, i, li, div, label, form, input, textarea, button')).forEach(element => {
         element.classList.toggle('dark-theme');
